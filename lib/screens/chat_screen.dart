fd6e67d7255cb034cc0232d99e7214a376731013
Tom Dinkelaker <tom.dinkelaker@gmail.com>
2020-05-30 12:38:20 +0200

    StreamBuilder enables a real time of a collection
    - The stream builder allows you to subscribe to a collection.
    - We use a firestore collection as the data source.

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -6,17 +6,33 @@ class ChatScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: ListView.builder(
-        itemBuilder: (ctxt, index) => Container(
-          padding: EdgeInsets.all(8),
-          child: Text('This works'),
-        ),
+      body: StreamBuilder(
+        stream: Firestore.instance
+            .collection('chats/B1AAdHxOosV3SoS7efbr/messages')
+            .snapshots(),
+        builder: (ctxt, streamSnapshot) {
+          if (streamSnapshot.connectionState == ConnectionState.waiting) {
+            return Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+          final documents = streamSnapshot.data.documents;
+          return ListView.builder(
+            itemCount: documents.length,
+            itemBuilder: (ctxt, index) {
+              return Container(
+                padding: EdgeInsets.all(8),
+                child: Text(documents[index]['text']),
+              );
+            },
+          );
+        },
       ),
       floatingActionButton: FloatingActionButton(
         child: Icon(Icons.add),
         onPressed: () {
           Firestore.instance
-              .collection('chats/9JWWERxxkY0TtEuCKAY2/messages')
+              .collection('chats/B1AAdHxOosV3SoS7efbr/messages')
               .snapshots()
               .listen(
             (event) {
