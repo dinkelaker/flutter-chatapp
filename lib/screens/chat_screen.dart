2bf4ffc46641b1e177c8104e5146e01d806288a6
Tom Dinkelaker <tom.dinkelaker@gmail.com>
2020-05-30 08:09:18 +0200

    Add firebase support
    - Add dependency for firestore
    - Workaround issue with .dex files (shrink code size, 64K limit)

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -1,12 +1,29 @@
 import 'package:flutter/material.dart';
 
+import 'package:cloud_firestore/cloud_firestore.dart';
+
 class ChatScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return ListView.builder(
-      itemBuilder: (ctxt, index) => Container(
-        padding: EdgeInsets.all(8),
-        child: Text('This works'),
+    return Scaffold(
+      body: ListView.builder(
+        itemBuilder: (ctxt, index) => Container(
+          padding: EdgeInsets.all(8),
+          child: Text('This works'),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: () {
+          Firestore.instance
+              .collection('chats/9JWWERxxkY0TtEuCKAY2/messages')
+              .snapshots()
+              .listen(
+            (event) {
+              print(event);
+            },
+          );
+        },
       ),
     );
   }
