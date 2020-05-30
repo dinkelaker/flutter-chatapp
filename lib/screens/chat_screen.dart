4e83c91e8672fb4874c9eb3661e26f3bbfc8af5d
Tom Dinkelaker <tom.dinkelaker@gmail.com>
2020-05-30 18:49:16 +0200

    Progress indicator and sign out
    - Sign out via firebase auth

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 import 'package:cloud_firestore/cloud_firestore.dart';
@@ -6,6 +7,38 @@ class ChatScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: Text('Flutter Chat'),
+        actions: <Widget>[
+          DropdownButton(
+            icon: Icon(
+              Icons.more_vert,
+              color: Theme.of(context).primaryIconTheme.color,
+            ),
+            items: [
+              DropdownMenuItem(
+                child: Container(
+                  child: Row(
+                    children: <Widget>[
+                      Icon(Icons.exit_to_app),
+                      SizedBox(
+                        width: 8,
+                      ),
+                      Text('Logout'),
+                    ],
+                  ),
+                ),
+                value: 'logout',
+              ),
+            ],
+            onChanged: (itemValue) {
+              if (itemValue == 'logout') {
+                FirebaseAuth.instance.signOut();
+              }
+            },
+          ),
+        ],
+      ),
       body: StreamBuilder(
         stream: Firestore.instance
             .collection('chats/B1AAdHxOosV3SoS7efbr/messages')
@@ -33,9 +66,7 @@ class ChatScreen extends StatelessWidget {
         onPressed: () {
           Firestore.instance
               .collection('chats/B1AAdHxOosV3SoS7efbr/messages')
-              .add({
-                'text': 'This was added by clicking the button.'
-              });
+              .add({'text': 'This was added by clicking the button.'});
         },
       ),
     );
