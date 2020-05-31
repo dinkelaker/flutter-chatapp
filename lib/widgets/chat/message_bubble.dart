f72cb6facec6f9bdbc9c95ae4f1cc558e316492f
Tom Dinkelaker <tom.dinkelaker@gmail.com>
2020-05-31 12:12:33 +0200

    Alignment of messages depending on user
    - for curent user the message are aligned right
    - Other user's message are aligned left

--- lib/widgets/chat/message_bubble.dart
+++ lib/widgets/chat/message_bubble.dart
@@ -8,27 +8,39 @@ class MessageBubble extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Row(children: [
-      Container(
-        decoration: BoxDecoration(
-          color: isCurrentUser ? Colors.grey[600] : Theme.of(context).accentColor,
-          borderRadius: BorderRadius.circular(12),
-        ),
-        width: 140,
-        padding: EdgeInsets.symmetric(
-          vertical: 10,
-          horizontal: 16,
-        ),
-        margin: EdgeInsets.symmetric(
-          vertical: 4,
-          horizontal: 8,
-        ),
-        child: Text(
-          message,
-          style: TextStyle(
-              color: Theme.of(context).accentTextTheme.headline1.color),
-        ),
-      ),
-    ]);
+    return Row(
+        mainAxisAlignment:
+            isCurrentUser ? MainAxisAlignment.end : MainAxisAlignment.start,
+        children: [
+          Container(
+            decoration: BoxDecoration(
+              color: isCurrentUser
+                  ? Colors.grey[600]
+                  : Theme.of(context).accentColor,
+              borderRadius: BorderRadius.only(
+                topLeft: Radius.circular(12),
+                topRight: Radius.circular(12),
+                bottomLeft:
+                    !isCurrentUser ? Radius.circular(0) : Radius.circular(12),
+                bottomRight:
+                    isCurrentUser ? Radius.circular(0) : Radius.circular(12),
+              ),
+            ),
+            width: 140,
+            padding: EdgeInsets.symmetric(
+              vertical: 10,
+              horizontal: 16,
+            ),
+            margin: EdgeInsets.symmetric(
+              vertical: 4,
+              horizontal: 8,
+            ),
+            child: Text(
+              message,
+              style: TextStyle(
+                  color: Theme.of(context).accentTextTheme.headline1.color),
+            ),
+          ),
+        ]);
   }
 }
